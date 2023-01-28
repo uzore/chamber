e51ebc98f0459118eb227a705b2ab6b60349c502
everyklothe <everyklothe@gmail.com>
2023-01-28 23:15:00 +0100

    Adjustment...

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => 
+{mainnav.classList.toggle('responsive')}, false);
+
+// What does toggle mean? Instead of add and remove, toggle means add the class name (the parameter, which in this case is named 'responsive') if it does not currently exist, and remove the 
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
